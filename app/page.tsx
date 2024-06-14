3984f816b2bc42eb86148c02ff2f3c956547fa7f
yuimorii <yuimoriistudio@gmail.com>
2024-06-13 22:16:29 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ const Home: React.FC = () => {
   return (
     <div className="bg-gray-900 text-white min-h-screen flex items-center justify-center">
       <main className="relative w-full h-full">
-        <CameraFeed modelPath="/models/tfjs_wizard/model.json" />
+        <CameraFeed modelPath="/models/mobilenet_docker/model.json" />
       </main>
     </div>
   );
