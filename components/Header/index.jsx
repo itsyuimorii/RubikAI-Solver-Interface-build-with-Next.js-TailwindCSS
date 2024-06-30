e227f190d6c696ef5b17dba7db27803fe8e5ab88
yuimorii <yuimoriistudio@gmail.com>
2024-06-30 16:53:31 -0500

    Update index.jsx

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -13,10 +13,7 @@ const Header = () => {
         <div className="bg-white fixed w-full box-border p-4">
             <div className="text-slate-800">
                 <Link href="/">Cuuuuuuube</Link>
-       
                 <div onClick={() => { setIsActive(!isActive) }} className="flex">
-  
-         
                     <motion.p variants={opacity} animate={!isActive ? "open" : "closed"}>Menu</motion.p>
                     <motion.p variants={opacity} animate={isActive ? "open" : "closed"}>Close</motion.p>
                 </div>
