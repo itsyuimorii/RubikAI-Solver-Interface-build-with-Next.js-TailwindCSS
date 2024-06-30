a95220b28a4a7667621efab3667c86cd9811a669
yuimorii <yuimoriistudio@gmail.com>
2024-06-30 12:35:42 -0500

    jsx

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Link from "next/link";
 import { motion, AnimatePresence } from 'framer-motion';
 import { opacity, background } from './animation';
@@ -12,12 +14,9 @@ const Header = () => {
             <div className="text-slate-800">
                 <Link href="/">Cuuuuuuube</Link>
        
-                <div onClick={() => { setIsActive(!isActive) }}>
-                          <div className="flex gap-8 cursor-pointer items-center justify-center">
-            <div className={`w-6 h-0.5 bg-black ${isActive ? 'transform rotate-45 -translate-y-1' : 'translate-y-0'}`}></div>
-            <div className={`w-6 h-0.5 bg-black ${isActive ? 'hidden' : ''}`}></div>
-            <div className={`w-6 h-0.5 bg-black ${isActive ? 'transform -rotate-45 translate-y-1' : 'translate-y-0'}`}></div>
-        </div>      
+                <div onClick={() => { setIsActive(!isActive) }} className="flex">
+  
+         
                     <motion.p variants={opacity} animate={!isActive ? "open" : "closed"}>Menu</motion.p>
                     <motion.p variants={opacity} animate={isActive ? "open" : "closed"}>Close</motion.p>
                 </div>
