ef45768651ca2b2dffcb41fc342fb97e004dcd4d
yuimorii <yuimoriistudio@gmail.com>
2024-07-07 17:12:23 -0500

    update

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -15,7 +14,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         {/* <Header /> */}
-        {children}</body>
-    </html>
+        {children}
+
+      </body >
+    </html >
   );
 }
