80b30faedafadf7948871ca0652a85c60c1ccbab
yuimorii <yuimoriistudio@gmail.com>
2024-07-13 21:55:26 -0500

    setup redux

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Provider } from 'react-redux';
+import store from '@/components/Store/store';
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -13,8 +15,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* <Header /> */}
-        {children}
+        <Provider store={store}>
+          {children}
+        </Provider>
 
       </body >
     </html >
