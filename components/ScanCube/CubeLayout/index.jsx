b68348bcd8f124ed511de293962defcce3b44c23
yuimorii <yuimoriistudio@gmail.com>
2024-07-21 22:17:02 -0500

    css........

--- components/ScanCube/CubeLayout/index.jsx
+++ components/ScanCube/CubeLayout/index.jsx
@@ -4,12 +4,12 @@ import CubeFace from "@/components/ScanCube/CubeLayout/cubeFace";
 //CubeLayout is a component that takes in a cubeArray prop and maps over it to render a CubeFace component for each face of the cube.
 const CubeLayout = (props) => {
     return ( 
-        <div className="grid grid-cols-4 gap-4 w-96">
+        <div className="grid grid-cols-4 gap-4 w-96 bg-black">
             {props.cubeArray.map((face, index) => {
                 // console.log(index, "index of face")
                 if (index === 0 || index === 5) {
                     return (
-                          <div key={index} className="grid grid-cols-subgrid gap-4 col-span-4">
+                          <div key={index} className="grid grid-cols-subgrid  gap-4 col-span-4">
                             <CubeFace  face={face} customClass="col-start-2" />
                         </div>
                     )
