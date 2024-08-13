22a609f0a355e5726e041ebd6bc9509a29ea4ad9
yuimorii <yuimoriistudio@gmail.com>
2024-08-12 21:29:08 -0500

    U

--- components/Description.jsx
+++ components/Description.jsx
@@ -3,8 +3,12 @@ import Link from 'next/link';
 
 export default function Description() {
     return (
-        <div className='flex justify-center my-40'>
-            <p className='font-ppneue-montreal font-semibold italic text-[5.5vw] text-center max-w-[60vw] leading-none'>Try  <Link href="https://www.tensorflow.org/js" className= 'text-blue-600 hover:underline'>Tensorflow.js</Link> to run Machine learning model in the broswer</p>
+        <section >
+        <div className='flex justify-center bg-[#c6ff55]'>
         </div>
+             <div className='flex justify-center my-20 bg'>
+            <p className='font-ppneue-montreal font-semibold italic text-[5.5vw] text-center max-w-[60vw] leading-none'>Try  <Link href="https://www.tensorflow.org/js" className= 'text-blue-600 hover:underline'>Tensorflow.js</Link> to run Machine learning model in the broswer</p>
+            </div>
+            </section>
     )
 }
