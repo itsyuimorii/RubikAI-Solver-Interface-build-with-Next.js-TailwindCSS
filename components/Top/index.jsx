89d5b3cc0976272ed13e1e6166fb78a355a80a31
yuimorii <yuimoriistudio@gmail.com>
2024-08-13 18:52:39 -0500

    Update index.jsx

--- components/Top/index.jsx
+++ components/Top/index.jsx
@@ -21,12 +21,12 @@ export default function Top() {
         transition={{ type: "tween", ease: "backOut", duration:0.5}}
       >
           <p onMouseEnter={() => {setIsHovered(true)}} onMouseLeave={() => {setIsHovered(false)}}>
-            A visual designer - with skills that haven't been replaced by A.I (yet) - making good shit only if the paycheck is equally good.
+           Check Jason Mayes's Machine Learning course - with skills that haven't been replaced by A.I (yet) - and learn how to solve a Rubik's Cube with A.I.
           </p>
       </motion.div>
 
       <div className={styles.body}>
-        <p>I'm a <span>selectively skilled</span> product designer with strong focus on producing high quality & impactful digital experience.</p>
+       <p>Have you ever thought about using AI to solve a <span>Rubik's Cube</span>? Explore how A.I. can master the art of solving this classic puzzle. </p>
       </div>
 
     </main>
